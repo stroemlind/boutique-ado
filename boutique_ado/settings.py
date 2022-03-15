bf732f88bccfee3a3a1a8fb1b664963fbd01b596
stroemlind <elin.stromlind@gmail.com>
2022-03-15 09:42:12 +0000

    Finish product size logic

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -156,7 +156,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 FREE_DELIVERY_THRESHOLD = 50
-STANDARD_DELIVERY_PRECENTAGE = 10
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
