6da780b3bf8807fa59afca9960f44471e89e6697
stroemlind <elin.stromlind@gmail.com>
2022-03-17 14:17:16 +0000

    Add basic checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,22 +1,41 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.context import bag_contents
 
+import stripe
 
 def checkout(request):
     """Checkout view"""
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing.')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KeIuBFRia4cbWFL5kaT7M9YdjyLuADOOmtWqxNUOJoQEM1da3eYDYtn5a2TA0hBwfYgL6umuWxB6fPa4quPDe6000s0yDmy2M',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
