98ce57ecdb2f17adcc59a7ce4756e6c67cbfeb9b
stroemlind <elin.stromlind@gmail.com>
2022-03-23 15:07:03 +0000

    Finish add product functionality

--- products/views.py
+++ products/views.py
@@ -57,6 +57,7 @@ def all_products(request):
 
     return render(request, 'products/products.html', context)
 
+
 def product_detail(request, product_id):
     """ A view to show individual product details """
 
@@ -71,7 +72,20 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ Add a product to the store """
-    form = ProductForm
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(
+                request,
+                'Failed to add product. Please ensure the form is valid.'
+            )
+    else:
+        form = ProductForm
+
     template = 'products/add_product.html'
     context = {
         'form': form,
