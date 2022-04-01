d8567016ca1f720d539c174f7bcc337cc0f90784
stroemlind <elin.stromlind@gmail.com>
2022-04-01 10:55:19 +0000

    Add env.py and stripe variables to file

--- bag/views.py
+++ bag/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404
+)
 from django.contrib import messages
 from products.models import Product
 
-# Create your views here.
+
 def view_bag(request):
     """ A view that renders the bag contents page """
     return render(request, 'bag/bag.html')
